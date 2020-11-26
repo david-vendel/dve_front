9346c4b8b305735bc57ac469ef46a08d2c145ca9
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-11-26 21:03:33 +0100

    missing page

---
+++ src/Pages/OchranaOsobnychUdajov.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OchranaOsobnychUdajov = () => {
+    return (
+        <div style={{ padding: 30 }}>
+            <h2> Ochrana Osobnych Udajov</h2>
+        </div>
+    );
+};
+
+export default OchranaOsobnychUdajov;
