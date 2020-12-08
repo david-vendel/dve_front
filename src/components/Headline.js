79000d942190c026d7bca19d65cfa19754dc6e26
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-12-08 16:49:27 +0100

    link added

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Headline = () => {
 
 return (
-        <div className="headline-main">
+        <div className="headline-main" onClick={() => {window.location.href="http://davidvendel.com"}}>
             <div className="headline-neon">
                 <span className="headline-text" data-text="">davidvendel.com</span>
                 <span className="headline-gradient"></span>
